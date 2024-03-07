4c17a4449b2c76a7642f03b6578e6a9b270e969c
CanseFr <jucansell@gmail.com>
2024-03-07 20:23:27 +0100

    :sparkles: Move file, and setup all reservations dto, service and controller, all tested with Postman.

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from "@nestjs/mongoose";
+import { ModelDefinition, MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://127.0.0.1/sleepr')]
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]){
+    return MongooseModule.forFeature(models)
+  }
+}
