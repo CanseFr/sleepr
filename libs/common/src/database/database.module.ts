7f021317005fb649014f699feb449523190bc776
CanseFr <jucansell@gmail.com>
2024-03-08 09:56:43 +0100

    :card_file_box: Change database related to name service for docker

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/sleepr')],
+  imports: [MongooseModule.forRoot('mongodb://mongo:27017/sleepr')],
 })
 export class DatabaseModule {
   static forFeature(models: ModelDefinition[]) {
