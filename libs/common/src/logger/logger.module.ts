3bd8200d057c192844bdd6214d17b002a6da51f2
CanseFr <jucansell@gmail.com>
2024-03-08 09:58:20 +0100

    :art: Ajust lint

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -1,19 +1,18 @@
-import { Module } from "@nestjs/common";
-import { LoggerModule as PinoLoggerModule } from "nestjs-pino";
+import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     PinoLoggerModule.forRoot({
       pinoHttp: {
         transport: {
-          target: "pino-pretty",
+          target: 'pino-pretty',
           options: {
-            singleLine: true
-          }
-        }
-      }
-    })
-  ]
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
 })
-export class LoggerModule {
-}
+export class LoggerModule {}
